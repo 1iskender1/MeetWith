6a17d7429523fae2d7fa90dc7ce5b71754d10a5f
sinemkaraoglu <104315797+sinemkaraoglu@users.noreply.github.com>
2022-06-05 19:16:08 +0300

    Add files via upload

---
+++ meet_with/lib/Screens/Login/auth.dart
@@ -0,0 +1,14 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+
+class AuthService {
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+  final FirebaseFirestore _firestore = FirebaseFirestore.instance;
+
+  //giriş yap fonksiyonu
+  Future<User?> signIn(String email, String password) async {
+    var user = await _auth.signInWithEmailAndPassword(
+        email: email, password: password);
+    return user.user;
+  }
+}
